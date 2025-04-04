c669b033c227a96c5bf3bb549b5a6ac9f33986e4
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2025-04-04 20:37:25 +0330

    Use schema instead of db model

--- routes/product.py
+++ routes/product.py
@@ -1,26 +1,26 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
-from models.product import Product
+from models import Product as ProductModel
+from schemas import Product as ProductSchema
 from database import get_db
 
 router = APIRouter()
 
-
-@router.get("/discounted-products", response_model=List[Product])
+@router.get("/discounted-products", response_model=List[ProductSchema])
 def get_discounted_products(
-        store: str = Query(None),
-        category: str = Query(None),
-        min_discount: float = Query(0),
-        db: Session = Depends(get_db)
+    store: Optional[str] = Query(None),
+    category: Optional[str] = Query(None),
+    min_discount: float = Query(0),
+    db: Session = Depends(get_db)
 ):
-    query = db.query(Product).filter(Product.discount_percent >= min_discount)
+    query = db.query(ProductModel).filter(ProductModel.discount_percent >= min_discount)
 
     if store:
-        query = query.filter(Product.store == store)
+        query = query.filter(ProductModel.store == store)
     if category:
-        query = query.filter(Product.category == category)
+        query = query.filter(ProductModel.category == category)
 
     products = query.all()
     return products
