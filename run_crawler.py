e240c2f5c00520fb59150b6998a7f437c1f4fa7f
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2025-04-04 22:00:01 +0330

    Create a main file for the crawler

---
+++ run_crawler.py
@@ -0,0 +1,17 @@
+from crawler import ZaraCrawler
+
+
+
+def main():
+    """
+    Main function to run the crawler.
+    """
+    # Initialize the crawler
+    crawler = ZaraCrawler()
+
+    # Run the crawler
+    crawler.run()
+
+
+if __name__ == "__main__":
+    main()
