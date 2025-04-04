c7632287676907c2858051d7ddf28c0bcc975dd7
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2025-04-04 20:36:59 +0330

    Add the product routes to the main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+# main.py
 from fastapi import FastAPI
+from routes.product import router as product_router
 
 app = FastAPI()
+
+app.include_router(product_router, prefix="", tags=["products"])
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
