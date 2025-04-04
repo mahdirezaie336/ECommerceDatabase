4a0bde969af7765c44c42e1dbc4c51c2da5ea7b1
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2025-04-04 20:23:17 +0330

    Create database config file

---
+++ database.py
@@ -0,0 +1,13 @@
+# database.py
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine
+from alembic.config import Config
+
+
+def get_db():
+    alembic_cfg = Config("alembic.ini")
+    url = alembic_cfg.get_main_option("sqlalchemy.url")
+    engine = create_engine(url)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = SessionLocal()
+    return db
