9a22cc4fcdda414589ea6e406c3498345c9af482
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2025-04-05 06:29:21 +0330

    Add method save_products to the crawler

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,2 +1,3 @@
 from .crawler import Crawler
 from .zara import ZaraCrawler
+from .amazon import AmazonCrawler
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from models import Product
+
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('AmazonCrawler')
 
 
 class Crawler(ABC):
     """
     Abstract base class for all crawlers.
     """
+
+    def save_products(self, products: List[Product]):
+        """
+        Save products to the database.
+        """
+        print("Saving products to the database...")
+        print(products)
+        pass
 
     @abstractmethod
     def run(self):
